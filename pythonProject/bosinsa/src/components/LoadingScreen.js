04a15714623a26dad6c45980ecaa32d1365f0894
0gonge <Challengef0802@gmail.com>
2024-12-03 05:48:40 +0900

    feat: sub 문구 추가 #3

--- pythonProject/bosinsa/src/components/LoadingScreen.js
+++ pythonProject/bosinsa/src/components/LoadingScreen.js
@@ -6,23 +6,37 @@ const LoadingScreen = () => {
     height: '100vh',
     backgroundColor: '#333',
     display: 'flex',
+    flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
     color: 'white',
-    fontSize: '48px',
-    fontWeight: 'bold',
+    textAlign: 'center',
   };
 
-  const textStyle = {
+  const titleStyle = {
     color: 'white',
     fontSize: '100px',
     fontWeight: 'bold',
     animation: 'fadeIn 3s ease-in-out',
   };
 
+  const subtitleStyle = {
+    color: 'white',
+    fontSize: '20px',
+    fontWeight: 'normal',
+    marginTop: '20px',
+    animation: 'fadeIn 3s ease-in-out',
+    animationDelay: '1s',
+    opacity: 0,
+    animationFillMode: 'forwards',
+  };
+
   return (
     <div style={containerStyle}>
-      <div style={textStyle}>BOSINSA</div>
+      <div style={titleStyle}>BOSINSA</div>
+      <div style={subtitleStyle}>
+        BOSINSA will be your personal coordinator.
+      </div>
       <style>
         {`
           @keyframes fadeIn {
