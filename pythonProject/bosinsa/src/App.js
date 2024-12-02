32df150cb025da13a95b9ea5680ad9b14765a93e
0gonge <Challengef0802@gmail.com>
2024-12-03 04:25:03 +0900

    feat: loading 화면 구현 #1

--- pythonProject/bosinsa/src/App.js
+++ pythonProject/bosinsa/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import LoadingScreen from './components/LoadingScreen';
 
-function App() {
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setIsLoading(false), 5000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      {isLoading ? (
+        <LoadingScreen />
+      ) : (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+            fontSize: '36px',
+            fontWeight: 'bold',
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          메인 화면
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ pythonProject/bosinsa/src/components/LoadingScreen.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const LoadingScreen = () => {
+  const containerStyle = {
+    width: '100vw',
+    height: '100vh',
+    backgroundColor: '#333',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: 'white',
+    fontSize: '48px',
+    fontWeight: 'bold',
+  };
+
+  const textStyle = {
+    color: 'white',
+    fontSize: '48px',
+    fontWeight: 'bold',
+    animation: 'fadeIn 3s ease-in-out',
+  };
+
+  return (
+    <div style={containerStyle}>
+      <div style={textStyle}>BOSINSA</div>
+      <style>
+        {`
+          @keyframes fadeIn {
+            0% {
+              opacity: 0;
+            }
+            100% {
+              opacity: 1;
+            }
+          }
+        `}
+      </style>
+    </div>
+  );
+};
+
+export default LoadingScreen;
